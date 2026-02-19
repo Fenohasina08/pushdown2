f3f54d2a95a893a181c0b7487cf469e8b05cfa03
fenohasina08 <fenohasinaherimamy@gmail.com>
2026-02-19 22:12:11 +0300

    initial commit

---
+++ src/main/resources/sql/schema.sql
@@ -0,0 +1,19 @@
+-- Type enum
+CREATE TYPE invoice_status AS ENUM ('DRAFT', 'CONFIRMED', 'PAID');
+
+-- Table invoice
+CREATE TABLE invoice (
+    id SERIAL PRIMARY KEY,
+    customer_name VARCHAR NOT NULL,
+    status invoice_status
+);
+
+-- Table invoice_line
+CREATE TABLE invoice_line (
+    id SERIAL PRIMARY KEY,
+    invoice_id INT NOT NULL REFERENCES invoice(id),
+    label VARCHAR NOT NULL,
+    quantity INT NOT NULL,
+    unit_price NUMERIC(10,2) NOT NULL
+);
+//sum case when
