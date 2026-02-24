f56b8bd6e6de3d4dd1493a1ee188e108d7534712
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-02-24 20:55:48 +0300

    VoteTypeCount content ok

--- src/main/java/VoteTypeCount.java
+++ src/main/java/VoteTypeCount.java
@@ -1,2 +1,33 @@
 public class VoteTypeCount {
+    private String voteType;
+    private long count;
+
+    public VoteTypeCount(String voteType, long count) {
+        this.voteType = voteType;
+        this.count = count;
+    }
+
+    public String getVoteType() {
+        return voteType;
+    }
+
+    public void setVoteType(String voteType) {
+        this.voteType = voteType;
+    }
+
+    public long getCount() {
+        return count;
+    }
+
+    public void setCount(long count) {
+        this.count = count;
+    }
+
+    @Override
+    public String toString() {
+        return "VoteTypeCount{" +
+                "count=" + count +
+                ", voteType='" + voteType + '\'' +
+                '}';
+    }
 }
