429983659b15265b68aa38bc07cba46e3e09a6d5
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-02-24 19:59:40 +0300

    feat: Q1 ok

--- src/main/java/DataRetriever.java
+++ src/main/java/DataRetriever.java
@@ -1,12 +1,22 @@
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.sql.ResultSet;
+import java.sql.*;
 
 public class DataRetriever {
     private DBConnection dbConnection;
 
     public DataRetriever(DBConnection dbConnection) {
         this.dbConnection = dbConnection;
+    }
+
+    public long countAllVotes() {
+        try (Connection conn = dbConnection.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT COUNT(id) AS total_votes FROM vote")) {
+            if (rs.next()) {
+                return rs.getLong("total_votes");
+            }
+            return 0L;
+        } catch (SQLException e) {
+            throw new RuntimeException("❌ Q1 Erreur", e);
+        }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        DataRetriever retriever = new DataRetriever();
+        DBConnection db = new DBConnection();
+        DataRetriever retriever = new DataRetriever(db);
 
-         System.out.println("=== Q1 ===");
-        retriever.findInvoiceTotals().forEach(System.out::println);
-
-         System.out.println("\n=== Q2 ===");
-        retriever.findConfirmedAndPaidInvoiceTotals().forEach(System.out::println);
-
-         System.out.println("\n=== Q3 ===");
-        InvoiceStatusTotals statusTotals = retriever.computeStatusTotals();
-        System.out.println(statusTotals);
-
-         System.out.println("\n=== Q4 ===");
-        double weighted = retriever.computeWeightedTurnover();
-        System.out.println("Chiffre d'affaires pondéré : " + weighted);
-
-        System.out.println("\n=== Q5-A ===");
-        retriever.findInvoiceTaxSummaries().forEach(System.out::println);
-
-        System.out.println("\n=== Q5-B ===");
-        double weightedTtc = retriever.computeWeightedTurnoverTtc();
-        System.out.println("Chiffre d'affaires TTC pondéré : " + weightedTtc);
-
+        long totalVote = retriever.countAllVotes();
+        System.out.println("totalVote=" + totalVote);
     }
 }
