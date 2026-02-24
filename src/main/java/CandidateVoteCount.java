63d51d52b3f5f521732ae725d52b7f0d4f867b36
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-02-24 21:19:30 +0300

    create  cndidatevotecount

---
+++ src/main/java/CandidateVoteCount.java
@@ -0,0 +1,46 @@
+import java.util.Objects;
+
+public class CandidateVoteCount {
+    private String candidateName;
+    private long validVoteCount;
+
+    public CandidateVoteCount(String candidateName, long validVoteCount) {
+        this.candidateName = candidateName;
+        this.validVoteCount = validVoteCount;
+    }
+
+    public String getCandidateName() {
+        return candidateName;
+    }
+
+    public void setCandidateName(String candidateName) {
+        this.candidateName = candidateName;
+    }
+
+    public long getValidVoteCount() {
+        return validVoteCount;
+    }
+
+    public void setValidVoteCount(long validVoteCount) {
+        this.validVoteCount = validVoteCount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        CandidateVoteCount that = (CandidateVoteCount) o;
+        return validVoteCount == that.validVoteCount && Objects.equals(candidateName, that.candidateName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(candidateName, validVoteCount);
+    }
+
+    @Override
+    public String toString() {
+        return "CandidateVoteCount{candidateName='" + candidateName + '\'' +
+                ", validVoteCount=" + validVoteCount +
+                '}';
+    }
+}
