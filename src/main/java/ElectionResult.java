4615733b66a0e7443f5811f159161c4966c6d31e
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-02-24 22:26:23 +0300

    create ElectionResult

---
+++ src/main/java/ElectionResult.java
@@ -0,0 +1,17 @@
+public class ElectionResult {
+    private String candidateName;
+    private long validVoteCount;
+
+    public ElectionResult(String candidateName, long validVoteCount) {
+        this.candidateName = candidateName;
+        this.validVoteCount = validVoteCount;
+    }
+
+    public String getCandidateName() {
+        return candidateName;
+    }
+
+    public long getValidVoteCount() {
+        return validVoteCount;
+    }
+}
