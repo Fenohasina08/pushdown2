262e50a78f370a48a9606abbba478ddecb624f63
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-02-24 19:36:21 +0300

    feat; dataretriever dbconnection implemented

--- src/main/java/DataRetriever.java
+++ src/main/java/DataRetriever.java
@@ -1,2 +1,12 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
 public class DataRetriever {
+    private DBConnection dbConnection;
+
+    public DataRetriever(DBConnection dbConnection) {
+        this.dbConnection = dbConnection;
+    }
 }
