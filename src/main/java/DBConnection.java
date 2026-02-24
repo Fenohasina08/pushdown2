ae50e9bcfd75e11b509c55f3a23ad68aeed785a6
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-02-24 19:25:01 +0300

    feat: add DBConnection

---
+++ src/main/java/DBConnection.java
@@ -0,0 +1,26 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class DBConnection {
+
+    public Connection getConnection() {
+        try {
+            String jdbcURl = System.getenv("JDBC_URl"); //
+            String user = System.getenv("postgres");
+            String password = System.getenv("fenohasina3123");
+            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/ceni", "postgres", "postgres");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
